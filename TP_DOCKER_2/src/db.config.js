c358990dd14a1fe6c7ffc88e9ea303ab3322c65a
Yanis Chad <yanis.chad@gmail.com>
2024-02-21 09:56:45 +0100

    Question 4

--- TP_DOCKER_2/src/db.config.js
+++ TP_DOCKER_2/src/db.config.js
@@ -1,16 +1,16 @@
-// Uncomment this block to use sqlite
-module.exports = {
-    dialect: "sqlite",
-    storage: "./my-db.sqlite",
-}
+// // Uncomment this block to use sqlite
+// module.exports = {
+//     dialect: "sqlite",
+//     storage: "./my-db.sqlite",
+// }
 
 // Uncomment this block to use mysql
-/*module.exports = {
-    hostname: "",
-    username: "",
-    password: "",
-    database: "",
+module.exports = {
+    hostname: "mysql",
+    username: "root",
+    password: "admin",
+    database: "mysql",
     port: 3306
-}*/
+}
 
 // TODO : adapt this file to load parameters from environment variables (process.env.VARIABLE_NAME)
